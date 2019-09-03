c365d3c52402c95afe927b0a9f69e03f47ebf29f
tania <tanislavens@yahoo.com>
2019-09-03 14:07:49 -0400

    added post and delete operations

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => {
-    res.status(200).json({ api: 'up...' });
-});
-
 const server = require('./server.js');
 
 const port = 8000;
--- server.js
+++ server.js
@@ -8,6 +8,9 @@ const express = require('express');
 
 const server = express();
 
+let movieId = 5;
+let actorId = 3;
+
 let actors = [
     {
         id: 1,
@@ -46,6 +49,12 @@ let movies = [
         released: true,
         rating: 5,
     },
+    {
+        id: 5,
+        name: "Zootopia",
+        released: true,
+        rating: 2,
+    },
 ];
 
 // sanity check endpoint
@@ -59,7 +68,45 @@ server.get('/api/movies', (req, res) => {
 });
 
 // as a user of this api, I want an endpoint to see a list of actors so that I can see all of the actors
+// teaches express to parse 
+server.use(express.json());
 
+// sanity check endpoint
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up...' });
+});
+
+server.get('/api/movies', (req, res) => {
+    const minRating = req.query.minrating;
+
+    let result = [...movies];
+
+    // if the client provides a minRating, filter the response
+    if (minRating) {
+        result = movies.filter(m => m.rating >= minRating);
+    }
+
+    res.status(200).json(movies);
+});
+
+server.post('/api/movies', (req, res) => {
+    const movie = req.body;
+
+    // add the new id
+    movie.id = movieId++;
+    movies.push(movie);
+
+    // return correct http status code for operation
+    res.status(201).json(movies);
+});
+
+server.delete('/api/movies/:id', (req, res) => {
+    const id = req.params.id;
+
+    movies = movies.filter(m => m.id !== Number(id));
+
+    res.status(200).json(movies);
+});
 
 // export default server
 module.exports = server;
